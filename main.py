0cb56cd935f14ea4460f518d522f386b41d01c30
indiko <126449012+indiko7777@users.noreply.github.com>
2023-12-17 12:58:57 +0100

    updated main

--- main.py
+++ main.py
@@ -16,10 +16,9 @@ def main():
         print("2. Process Speeches")
         print("3. Search president who speak the most about a word'")
         print("4. Test Question Functions")
-        print("5. Check Most Relevant Document")
-        print("6. Ask a Question to the chat-bot")
-        print("7. Display TF-IDF Matrix")
-        print("8. Exit")
+        print("5. Ask a Question to the chat-bot")
+        print("6. Display TF-IDF Matrix")
+        print("7. Exit")
         print("=====================")
         choice = input("Enter your choice (1-8): ")
 
@@ -32,17 +31,15 @@ def main():
         elif choice == '4':
             test_question_functions()
         elif choice == '5':
-            check_most_relevant_document_menu(tfidf_matrix, idf_scores)
-        elif choice == '6':
             speeches_dir = 'speeches'  # Modify this according to your directory structure
             question = input("Enter your question: ")
             print("Question:", question)
             response_lines = find_response_for_question(tfidf_matrix, idf_scores, speeches_dir, question)
             print("Response for the question:")
             print(response_lines)
-        elif choice == '7':
+        elif choice == '6':
             display_tfidf_matrix(tfidf_matrix)
-        elif choice == '8':
+        elif choice == '7':
             print("Exiting the program.")
             break
         else:
